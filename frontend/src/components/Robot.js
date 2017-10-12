e992ee20128b95a7ee97ab6086a8c4cf7bb6e723
Stephen Boynton <boyntonsc@gmail.com>
2017-10-12 11:51:13 -0400

    Setup health player state

--- frontend/src/components/Robot.js
+++ frontend/src/components/Robot.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/Robot.css";
 
 export default function Robot(props) {
   return (
     <div className="Robot">
-      <img src={props.image} />
-      <h3>{props.name}</h3>
+      <img src={props.robot.image} />
+      <h3>{props.robot.name}</h3>
     </div>
   );
 }
