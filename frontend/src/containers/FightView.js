ca1e0393c62a25918dbfd04884f7d3c971527a5a
Stephen Boynton <boyntonsc@gmail.com>
2017-10-12 15:28:25 -0400

    Async timing functions

--- frontend/src/containers/FightView.js
+++ frontend/src/containers/FightView.js
@@ -8,11 +8,17 @@ import {
   punchChanceToHit,
   bigPunchChanceToHit,
   blockChanceToHit
-} from "../fightHelpers";
+} from "../helpers/fightHelpers";
+import {
+  oneSecondDelay,
+  twoSecondDelay,
+  threeSecondDelay
+} from "../helpers/delayHelpers";
 import "../styles/FightView.css";
 
 export default class FightView extends Component {
   state = {
+    newRound: true,
     fighters: [
       {
         fighterType: "player",
@@ -26,7 +32,7 @@ export default class FightView extends Component {
         hp: 4,
         maxHp: 4,
         image: "https://robohash.org/IGX.png?set=set1",
-        name: "Whatever"
+        name: "MAXIMUM_Max"
       }
     ],
     moves: ["Chop", "Roundhouse Kick", "Block"],
@@ -39,6 +45,30 @@ export default class FightView extends Component {
   };
 
   ////////////// Handler Functions  //////////////////////
+  _fakeFetch = async () => {
+    await threeSecondDelay();
+    this.setState({
+      newRound: false
+    });
+  };
+
+  componentDidMount() {
+    // fake fetch timing
+    this._fakeFetch();
+  }
+
+  _handleNewRound = () => {
+    if (this.state.newRound) {
+      return (
+        <RoundOverlay
+          image={this.state.fighters[1].image}
+          round={this.state.game.round}
+        />
+      );
+    } else {
+      return;
+    }
+  };
 
   _handleClick = evt => {
     this._handleMove(evt.target.value);
@@ -48,44 +78,34 @@ export default class FightView extends Component {
     switch (moveName) {
       case "Chop":
         if (punchChanceToHit()) {
-          
-          console.log("chop")
+          console.log("chop");
         } else {
-
         }
         break;
       case "Roundhouse Kick":
         if (bigPunchChanceToHit()) {
-
         } else {
-
         }
         break;
       case "Block":
         if (blockChanceToHit()) {
-
         } else {
-
         }
         break;
     }
-    this.setState((prevState)=>{
-      return {playerTurn: !prevState.playerTurn}
+    this.setState(prevState => {
+      return { playerTurn: !prevState.playerTurn };
     });
   };
 
-  componentDidUpdate(){
-    console.log(this.state.playerTurn)
+  componentDidUpdate() {
+    console.log(this.state.playerTurn);
   }
 
   render() {
-
     return (
       <div className="FightView">
-        <RoundOverlay
-          image={this.state.fighters[1].image}
-          round={this.state.game.round}
-        />
+        {this._handleNewRound()}
         <HealthDisplay players={this.state.fighters} status={this.state.game} />
         <Robot robot={this.state.fighters[1]} />
         {/* <FightLog /> */}
