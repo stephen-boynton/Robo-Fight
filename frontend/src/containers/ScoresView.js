390bf16a2b443db90e21cde5b82eec214e7ae18d
Stephen Boynton <boyntonsc@gmail.com>
2017-10-12 12:50:01 -0400

    Sweet, sweet stylin'

--- frontend/src/containers/ScoresView.js
+++ frontend/src/containers/ScoresView.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import ScoreBoard from "../components/ScoreBoard";
-
-
+import "../styles/ScoresView.css";
 
 export default class ScoreView extends Component {
-
-
   render() {
     return (
       <div className="ScoreView">
