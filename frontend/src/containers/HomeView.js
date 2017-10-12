95200467d45b977774b3350291c2e238a93da408
lotushack <alonyalafayette1@gmail.com>
2017-10-12 14:59:23 -0400

    pull

--- frontend/src/containers/HomeView.js
+++ frontend/src/containers/HomeView.js
@@ -2,25 +2,7 @@ import React, { Component } from "react";
 import "../styles/HomeView.css";
 
 export default class HomeView extends Component {
-<<<<<<< HEAD
-  constructor() {
-    super();
-};
-  _handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log(this.props.player);
-    fetch("/fight")
-
-  };
-
-  render(){
-    return(
-      <div className="HomeView">
-        <form className="form" onSubmit={this._handleSubmit}>
-            <label  className="username-label">Enter your username:</label>
-            <input type="username" className={this.props.player} id="username"/>
-            <button type="submit" className="button" id="button">Start</button>
-=======
+   
   render() {
     return (
       <div className="HomeView">
@@ -42,7 +24,6 @@ export default class HomeView extends Component {
           <button type="button" className="form-button" id="button">
             Start
           </button>
->>>>>>> e936cdc1508d6bdb83788ecd23aeebe853efac16
         </form>
         <div className="instructions-container">
           <p>Instructions go here...</p>
