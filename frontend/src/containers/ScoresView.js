6c1457a2abd4d774961079d28dc8f21156ed3ec0
Nick Nauert <nicknauert@gmail.com>
2017-10-12 10:52:14 -0400

    scoreboard expanded, light styling

--- frontend/src/containers/ScoresView.js
+++ frontend/src/containers/ScoresView.js
@@ -9,6 +9,7 @@ export default class ScoreView extends Component {
   render() {
     return (
       <div className="ScoreView">
+        <h2>HIGH SCORES</h2>
         <ScoreBoard />
       </div>
     );
